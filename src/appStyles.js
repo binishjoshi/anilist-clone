1ed3f0ab3190b5c98a3e3235b195a0291b41b397
Binish Joshi <binishjoshi@protonmail.com>
2020-09-16 20:29:48 +0545

    Redefined dark theme settings

--- src/appStyles.js
+++ src/appStyles.js
@@ -3,5 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const darkTheme = createMuiTheme({
   palette: {
     type: 'dark',
+    background: {
+      default: '#0b1622',
+      paper: '#151f2e',
+    }
   },
 });

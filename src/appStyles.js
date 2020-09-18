1bd3317a4a2a3e93a1a1e427c571d59eb734d240
Binish Joshi <binishjoshi@protonmail.com>
2020-09-18 15:44:18 +0545

    Added button override

--- src/appStyles.js
+++ src/appStyles.js
@@ -8,4 +8,15 @@ export const darkTheme = createMuiTheme({
       paper: '#151f2e',
     }
   },
+  overrides: {
+    MuiButton: {
+      contained: {
+        backgroundColor: '#151f2e',
+        color: '#c9d7e3',
+        "&:hover": {
+          backgroundColor: '#151f2e',
+        }
+      }
+    }
+  }
 });

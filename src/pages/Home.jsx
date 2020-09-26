929a518987fb52966274fd96f4aec2f336b538ef
Binish Joshi <binishjoshi@protonmail.com>
2020-09-26 16:17:52 +0545

    Added search to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeCard from '../components/HomeCard/HomeCard';
+import SearchAnime from './SearchAnime';
 
 const Home = () => {
   return (
     <div>
       <HomeCard />
+      <SearchAnime />
     </div>
   );
 };
